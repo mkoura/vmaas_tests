59a0ec40f9fc6a3f9ad43993ef04d0a2a8300b17
Patrik Segedy <psegedy@redhat.com>
2018-06-12 16:28:34 +0200

    Test that response is same - GH#306

--- vmaas/tests/test_updates.py
+++ vmaas/tests/test_updates.py
@@ -11,7 +11,8 @@ from vmaas.utils.blockers import GH
 class TestUpdatesAll(object):
     def test_post_multi(self, rest_api):
         """Tests updates using POST with multiple packages."""
-        request_body = tools.gen_updates_body([p[0] for p in packages.PACKAGES])
+        request_body = tools.gen_updates_body(
+            [p[0] for p in packages.PACKAGES])
         updates = rest_api.get_updates(body=request_body).response_check()
         schemas.updates_top_schema.validate(updates.raw.body)
         assert len(updates) == len(packages.PACKAGES)
@@ -64,7 +65,8 @@ class TestUpdatesInRepos(object):
     def test_post_single(self, rest_api, package_record):
         """Tests updates in repos using POST with single package."""
         name, expected_updates = package_record
-        request_body = tools.gen_updates_body([name], repositories=packages.REPOS)
+        request_body = tools.gen_updates_body(
+            [name], repositories=packages.REPOS)
         updates = rest_api.get_updates(body=request_body).response_check()
         schemas.updates_top_repolist_schema.validate(updates.raw.body)
         assert len(updates) == 1
@@ -77,7 +79,8 @@ class TestUpdatesInRepos(object):
     def test_post_nonexistent_repo(self, rest_api):
         """Tests updates in repos using POST with single package."""
         name = packages.PACKAGES_W_REPOS[0][0]
-        request_body = tools.gen_updates_body([name], repositories=['nonexistent-1'])
+        request_body = tools.gen_updates_body(
+            [name], repositories=['nonexistent-1'])
         updates = rest_api.get_updates(body=request_body).response_check()
         assert not updates
 
@@ -87,7 +90,7 @@ class TestUpdatesFilterRelease(object):
     def test_post_multi(self, rest_api):
         """Tests updates with filtered release version using POST with multiple packages."""
         request_body = tools.gen_updates_body(
-            [p[0] for p in packages.PACKAGES_RELEASE_FILTER], releasever='7')
+            [p[0] for p in packages.PACKAGES_RELEASE_FILTER], releasever='6')
         updates = rest_api.get_updates(body=request_body).response_check()
         schemas.updates_top_releasever_schema.validate(updates.raw.body)
         assert len(updates) == len(packages.PACKAGES_RELEASE_FILTER)
@@ -105,7 +108,7 @@ class TestUpdatesFilterRelease(object):
     def test_post_single(self, rest_api, package_record):
         """Tests updates with filtered release version using POST with single package."""
         name, expected_updates = package_record
-        request_body = tools.gen_updates_body([name], releasever='7')
+        request_body = tools.gen_updates_body([name], releasever='6')
         updates = rest_api.get_updates(body=request_body).response_check()
         schemas.updates_top_releasever_schema.validate(updates.raw.body)
         assert len(updates) == 1
@@ -146,3 +149,26 @@ class TestUpdatesFilterBasearch(object):
         tools.validate_package_updates(package, expected_updates)
         for update in package.available_updates:
             assert update['basearch'] == request_body['basearch']
+
+
+class TestUpdatesDiff(object):
+    def test_post_diff(self, make_conn):
+        """Tests that application returns always the same response using POST."""
+        request_body = tools.gen_updates_body(
+            ['bash-0:4.2.46-20.el7_2.x86_64'])
+        rest_api = make_conn()
+        init = rest_api.get_updates(body=request_body).response_check()
+        for i in range(100):
+            rest_api = make_conn()
+            response = rest_api.get_updates(body=request_body).response_check()
+            assert init.raw.body == response.raw.body
+
+    def test_get_diff(self, make_conn):
+        """Tests that application returns always the same response using GET."""
+        pkg = 'bash-0:4.2.46-20.el7_2.x86_64'
+        rest_api = make_conn()
+        init = rest_api.get_update(pkg).response_check()
+        for i in range(100):
+            rest_api = make_conn()
+            response = rest_api.get_update(pkg).response_check()
+            assert init.raw.body == response.raw.body
