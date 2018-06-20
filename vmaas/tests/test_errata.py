3bee3e6518d0788e6e433587a27883c0703cdbd8
Patrik Segedy <psegedy@redhat.com>
2018-06-20 13:44:45 +0200

    Errata negative testing

--- vmaas/tests/test_errata.py
+++ vmaas/tests/test_errata.py
@@ -10,12 +10,19 @@ ERRATA = [
     ('vmaas_test_2', 'vmaas_test_2'),
 ]
 
+ERRATA_NEG = [
+    ('RHSA-9999-9999', None),
+    ('', None)
+]
+
 ERRATA_SMOKE = [
     ('RHBA-2016:2858', None),
     ('RHSA-2017:1931', None),
     ('RHSA-2018:1099', 'RHSA-2018:1099'),
     ('RHEA-2010:0932', None),   # GH#310
-    ('RHBA-2016:1031', None)    # GH#310
+    ('RHBA-2016:1031', None),    # GH#310
+    ('RHSA-9999-9999', None),
+    ('', None)
 ]
 
 ERRATA_REGEX = [
@@ -34,19 +41,26 @@ class TestErrataQuery(object):
         errata_response = rest_api.get_errata(
             body=request_body).response_check()
         schemas.errata_schema.validate(errata_response.raw.body)
-        assert len(errata_response) == len(errata)
+        exp_errata = [x for x in errata if x not in ERRATA_NEG]
+        assert len(errata_response) == len(exp_errata)
         for erratum_name, __ in errata:
             assert erratum_name in errata_response
 
     def post_single(self, rest_api, erratum):
         """Tests single erratum using POST."""
         erratum_name, _ = erratum
-        request_body = tools.gen_errata_body([erratum_name])
+        if not erratum_name:
+            request_body = tools.gen_errata_body([])
+        else:
+            request_body = tools.gen_errata_body([erratum_name])
         errata = rest_api.get_errata(body=request_body).response_check()
-        schemas.errata_schema.validate(errata.raw.body)
-        assert len(errata) == 1
-        erratum, = errata
-        assert erratum.name == erratum_name
+        if erratum_name in [e[0] for e in ERRATA_NEG]:
+            assert not errata
+        else:
+            schemas.errata_schema.validate(errata.raw.body)
+            assert len(errata) == 1
+            erratum, = errata
+            assert erratum.name == erratum_name
 
     def test_post_multi(self, rest_api):
         """Tests multiple test errata using POST."""
@@ -78,11 +92,17 @@ class TestErrataQuery(object):
         erratum_name, _ = erratum
         if erratum_name in ['RHEA-2010:0932', 'RHBA-2016:1031'] and GH(310).blocks:
             pytest.skip("Blocked by GH#310")
-        errata = rest_api.get_erratum(erratum_name).response_check()
-        schemas.errata_schema.validate(errata.raw.body)
-        assert len(errata) == 1
-        erratum, = errata
-        assert erratum.name == erratum_name
+        if not erratum_name:
+            rest_api.get_erratum(erratum_name).response_check(405)
+        else:
+            errata = rest_api.get_erratum(erratum_name).response_check()
+            if erratum_name in [e[0] for e in ERRATA_NEG]:
+                assert not errata
+            else:
+                schemas.errata_schema.validate(errata.raw.body)
+                assert len(errata) == 1
+                erratum, = errata
+                assert erratum.name == erratum_name
 
 
 class TestErrataModifiedSince(object):
