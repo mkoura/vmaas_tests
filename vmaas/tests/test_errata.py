9fba055e33b3c951fa614d15a05205f1397a20af
Patrik Segedy <psegedy@redhat.com>
2018-06-22 13:21:21 +0200

    Behaviour was changed - same as for CVEs now
    https://github.com/RedHatInsights/vmaas/commit/5dae30c5da257aa69d0a677a1fda3496bd300d2d

--- vmaas/tests/test_errata.py
+++ vmaas/tests/test_errata.py
@@ -34,8 +34,8 @@ ERRATA_SMOKE = [
 ]
 
 ERRATA_REGEX = [
-    ('vmaas', 9),
-    ('vmaas*', 9),
+    ('vmaas', 0),
+    ('vmaas.*', 9),
     ('RHSA-2018:\\d+', 44),
     ('RHSA-2018:015[1-5]', 1),
     ('RH.*', 573)      # GH#310
@@ -182,8 +182,9 @@ class TestErrataRegex(object):
             pytest.skip("Blocked by GH#310")
         request_body = tools.gen_errata_body([erratum_name])
         errata = rest_api.get_errata(body=request_body).response_check()
-        schemas.errata_schema.validate(errata.raw.body)
         assert len(errata) == errata_num
+        if errata_num > 0:
+            schemas.errata_schema.validate(errata.raw.body)
 
     @pytest.mark.parametrize(
         'erratum', ERRATA_REGEX, ids=[e[0] for e in ERRATA_REGEX])
@@ -193,5 +194,6 @@ class TestErrataRegex(object):
         if erratum_name in ['RH.*'] and GH(310).blocks:
             pytest.skip("Blocked by GH#310")
         errata = rest_api.get_erratum(erratum_name).response_check()
-        schemas.errata_schema.validate(errata.raw.body)
         assert len(errata) == errata_num
+        if errata_num > 0:
+            schemas.errata_schema.validate(errata.raw.body)
